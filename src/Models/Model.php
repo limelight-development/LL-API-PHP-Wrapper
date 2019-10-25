ca8aec9997fb36f2985b5262b484e2e9a4ec39ea
John Internet <jonjon1234.github@gmail.com>
2019-10-25 12:47:24 +0100

    Added substantiate to auto-fill class arrays.

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -11,4 +11,11 @@ class Model implements ClientAware {
 	private $client;
 	public function setClient(APIClient $client){$this->client = $client;}
 	public function getClient(): APIClient {return $this->client;}
+
+	public static function substantiate(string $class, array $data, APIClient $api){
+		return array_map(
+			$class . '::fromObject', $data,
+			array_fill(0, count($data), $api)
+		);
+	}
 }
